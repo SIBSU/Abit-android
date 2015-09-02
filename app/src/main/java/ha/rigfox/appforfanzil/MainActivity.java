f0caff890cfe15072e26983fcbbb96903fda84cd
flameous <kh.fanzil@gmail.com>
2015-09-03 03:38:10 +0500

    govnocode

--- app/src/main/java/ha/rigfox/appforfanzil/MainActivity.java
+++ app/src/main/java/ha/rigfox/appforfanzil/MainActivity.java
@@ -1,5 +1,5 @@
 package ha.rigfox.appforfanzil;  //test-lol
-                                //hello from GitHUB
+                                //hello from Github!
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
